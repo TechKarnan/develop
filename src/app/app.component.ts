4591d7aa2bd5315b6e60276d5b37a4b1be4e3937
preetham <preetham7080@gmail.com>
2021-10-07 10:59:26 +0530

    last commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myproject';
+  title = 'Preetham';
   show="red";
   color="red";
   names=["preetham","prajwal","gokul"];
